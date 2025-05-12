1ae2370dfb16f3434ba77f930680db1c65f7628e
paul4coding <tcheouafeipaulin@gmail.com>
2025-05-12 14:42:06 +0000

    Adaption de la page d'inscription en fonction du profil choisis 	modified:   healthSync/firstApp/__pycache__/forms.cpython-312.pyc 	modified:   healthSync/firstApp/forms.py 	modified:   healthSync/firstApp/templates/user_template/inscription.html

--- healthSync/firstApp/forms.py
+++ healthSync/firstApp/forms.py
@@ -7,7 +7,21 @@ class ConnexionForm(forms.Form):
 
 class InscriptionForm(forms.ModelForm):
     mot_de_passe = forms.CharField(widget=forms.PasswordInput)
+    user_type = forms.ChoiceField(
+        choices=[('patient', 'Patient'), ('personnel', 'Personnel de santé')],
+        widget=forms.RadioSelect,
+        label="Qui suis-je ?"
+    )
+    personnel_role = forms.ChoiceField(
+        choices=[
+            ('medecin', 'Médecin'),
+            ('laborantin', 'Laborantin'),
+            ('secretaire', 'Secrétaire')
+        ],
+        required=False,
+        label="Quel est votre rôle ?"
+    )
 
     class Meta:
         model = Utilisateur
-        fields = ['nom', 'prenom', 'sexe', 'age', 'email', 'mot_de_passe']
+        fields = ['nom', 'prenom', 'sexe', 'age', 'email', 'mot_de_passe', 'user_type', 'personnel_role']

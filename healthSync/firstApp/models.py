2de73f8a5f51d20a901705f4b8a7d05cc0f30f76
paul4coding <tcheouafeipaulin@gmail.com>
2025-05-13 14:37:59 +0000

    ajout des model medecin laborantin secretaire 	modified:   healthSync/firstApp/__pycache__/models.cpython-312.pyc 	new file:   healthSync/firstApp/migrations/0002_laborantin_patient_secretaire_specialite_medecin.py 	new file:   healthSync/firstApp/migrations/__pycache__/0002_laborantin_patient_secretaire_specialite_medecin.cpython-312.pyc 	modified:   healthSync/firstApp/models.py

--- healthSync/firstApp/models.py
+++ healthSync/firstApp/models.py
@@ -1,9 +1,8 @@
+
 from django.db import models
 
-# Create your models here.
-
-
 class Utilisateur(models.Model):
+    """Classe parent pour tous les types d'utilisateurs."""
     nom = models.CharField(max_length=100)
     prenom = models.CharField(max_length=100)
     sexe = models.CharField(max_length=1, choices=[('M', 'Masculin'), ('F', 'Féminin')])
@@ -13,3 +12,26 @@ class Utilisateur(models.Model):
 
     def __str__(self):
         return f"{self.prenom} {self.nom}"
+
+class Specialite(models.Model):
+    """Classe pour les spécialités médicales."""
+    nom = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.nom
+
+class Patient(Utilisateur):
+    """Classe pour les patients."""
+    numero_carte_identite = models.CharField(max_length=50, unique=True)
+
+class Laborantin(Utilisateur):
+    """Classe pour les laborantins."""
+
+
+class Medecin(Utilisateur):
+    """Classe pour les médecins."""
+    specialite = models.ForeignKey(Specialite, on_delete=models.CASCADE)
+
+class Secretaire(Utilisateur):
+    """Classe pour les secrétaires."""
+

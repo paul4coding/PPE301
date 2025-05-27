eb24383eac230495116ae9813b902de95d44a90c
paul4coding <tcheouafeipaulin@gmail.com>
2025-05-27 16:38:53 +0000

    adaptation du dashbord patient: 	modified:   healthSync/firstApp/__pycache__/views.cpython-312.pyc 	modified:   healthSync/firstApp/templates/admin_template/html/hos-patient-dash.html 	modified:   healthSync/firstApp/views.py

--- healthSync/firstApp/views.py
+++ healthSync/firstApp/views.py
@@ -3,6 +3,7 @@ from .forms import ConnexionForm, InscriptionForm
 from .models import Utilisateur, Patient, Laborantin, Medecin, Secretaire
 from django.views.decorators.http import require_POST
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 # Fonction utilitaire pour déterminer le rôle d'un utilisateur
 def get_user_role(user):
     if hasattr(user, 'medecin'):
@@ -251,8 +252,13 @@ def hos_all_patients(request):
     return render(request, "admin_template/html/hos-all-patients.html", get_admin_context(request))
 def hos_book_appointment(request):
     return render(request, "admin_template/html/hos-book-appointment.html", get_admin_context(request))
+
+@login_required
 def hos_doctor_dash(request):
-    return render(request, "admin_template/html/hos-doctor-dash.html", get_admin_context(request))
+    patient = request.user  # ou request.user.patient selon ton modèle
+    return render(request, 'admin_template/html/hos-patient-dash.html', {'patient': patient})
+
+
 def hos_doctor_profile(request):
     return render(request, "admin_template/html/hos-doctor-profile.html", get_admin_context(request))
 def hos_edit_doctor(request):

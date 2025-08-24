e65fc0fa648ac36c606dce121486f4ef5b45912c
leroypn <panaleroy@gmail.com>
2025-08-24 17:20:32 +0000

    arrangement 	modified:   healthSync/firstApp/__pycache__/utils.cpython-312.pyc 	modified:   healthSync/firstApp/utils.py

--- healthSync/firstApp/utils.py
+++ healthSync/firstApp/utils.py
@@ -1,11 +1,15 @@
 from django.core.mail import send_mail
+from datetime import datetime
 
 def envoyer_notification_rdv(rdv):
-    sujet = f"Confirmation de rendez-vous - {rdv.date.strftime('%d/%m/%Y %H:%M')}"
+    # Combiner la date et l'heure
+    date_heure = datetime.combine(rdv.date, rdv.heure)
+
+    sujet = f"Confirmation de rendez-vous - {date_heure.strftime('%d/%m/%Y %H:%M')}"
     message = (
         f"Bonjour {rdv.patient.nom},\n\n"
         f"Votre rendez-vous avec le Dr. {rdv.medecin.nom} est bien enregistré "
-        f"pour le {rdv.date.strftime('%d/%m/%Y à %H:%M')}.\n\n"
+        f"pour le {date_heure.strftime('%d/%m/%Y à %H:%M')}.\n\n"
         "Merci d'arriver 10 minutes en avance.\n\n"
         "Cordialement,\nL'équipe HealthSync"
     )
